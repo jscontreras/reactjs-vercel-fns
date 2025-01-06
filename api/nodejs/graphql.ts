e12ca5ae54732fd3a6136264875d0a13993a1df5
Sebastian Contreras <js.contreras@outlook.com>
2025-01-06 10:34:37 -0500

    Adding graphql

---
+++ api/nodejs/graphql.ts
@@ -0,0 +1,32 @@
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+
+// Define your GraphQL schema
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
+
+// Define your resolvers
+const resolvers = {
+  Query: {
+    hello: () => 'Hello from Vercel Function!',
+  },
+};
+
+// Create Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Create handler for Vercel Function
+export default startServerAndCreateNextHandler(server);
+
+/*
+curl --request POST \
+    --header 'content-type: application/json' \
+    --url http://localhost:3000/api/nodejs/graphql \
+    --data '{"query":"query ExampleQuery {\n  hello\n}"}'
+*/
